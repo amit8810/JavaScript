a017f2c3329135cfa6c70559e772969d1ab3d956
amit8810 <ac446643@gmail.com>
2023-10-07 16:40:49 +0530

    dataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+// console.log(3 + 3); console.log("Amit") // not good for readability
+
+let name = "Amit" // string
+let age = 18 // integer
+let isLoggesIn = false // boolean
+let state = null
+let score
+
+console.log(typeof score)
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
+
+
+
+
