26024e79bef1d4b164fd1157368e41c6a5855a20
amit8810 <ac446643@gmail.com>
2023-10-07 17:30:08 +0530

    comparision in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1) // true
+// console.log(2 >= 1) // true
+// console.log(2 < 1) // false
+// console.log(2 == 1) // false
+// console.log(2 != 1) // true
+
+//! Comparing Different dataTypes
+// console.log("2" > 1); // true  // Avoid all this
+// console.log("02" > 1) // true
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+
+//* # Strict Check
+console.log(2 === "2") // false // check value and datatype both
+console.log(2 == "2") // true // check only value
+
+
+
+
+
+
