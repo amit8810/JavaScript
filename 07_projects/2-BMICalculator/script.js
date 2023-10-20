030b3183dd59ad45855e5337805278a1e9ad0856
amit8810 <ac446643@gmail.com>
2023-10-20 20:14:45 +0530

    project 2 completed

--- 07_projects/2-BMICalculator/script.js
+++ 07_projects/2-BMICalculator/script.js
@@ -1,41 +1,3 @@
-#Project related to DOM
-
-## project link
-[click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution code
-
-## project 1 solutions
-
-```javascript
-console.log("amit")
-const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body");
-
-buttons.forEach(function (button) {
-  //   console.log(button);
-  button.addEventListener("click", function (e) {
-    // console.log(e);
-    // console.log(e.target);
-    if (e.target.id === "grey") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "white") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "yellow") {
-      body.style.backgroundColor = e.target.id;
-    }
-  });
-});
-
-```
-## project 2 solution
-
-```javascript
 const form = document.querySelector("form");
 // this usecase will give you empty
 // const height = parseInt(document.querySelector("#height").value);
@@ -64,4 +26,3 @@ form.addEventListener("submit", function (e) {
     }
   }
 });
-```
